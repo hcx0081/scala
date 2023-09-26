9522a259e6d163b95e30b4333c9692d920df7d0c
hcx0081 <623363564@qq.com>
2023-09-26 17:56:15 +0800

    首次提交

---
+++ hello/src/main/java/com/scala/MainJava.java
@@ -0,0 +1,10 @@
+package com.scala;
+
+/**
+ * {@code @description:} ${description}
+ */
+public class MainJava {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ hello/src/main/scala/com/scala/MainScala.scala
@@ -0,0 +1,10 @@
+package com.scala
+
+/**
+ * {@code @description:} ${description}
+ */
+object MainScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
