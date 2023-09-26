61034c7bf118790956cbcfec711717edfd7ae9f7
root <623363564@qq.com>
2023-09-27 00:58:02 +0800

    类和对象

---
+++ hello/src/main/scala/com/scala/clzAndObj/Person.scala
@@ -0,0 +1,25 @@
+package com.scala.clzAndObj
+
+/**
+ * {@code @description:} ${description}
+ */
+class Person {
+  private var _name = "zs"
+  private var _age = 20
+
+  // Getter方法
+  def name: String = _name
+
+  // Setter方法
+  def name_=(newValue: String): Unit = {
+    _name = newValue
+  }
+
+  // Getter方法
+  def age: Int = _age
+
+  // Setter方法
+  def age_=(newValue: Int): Unit = {
+    _age = newValue
+  }
+}
