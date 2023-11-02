dbf51c0e510731142d3d8f045993a62e89c33383
hcx0081 <623363564@qq.com>
2023-11-02 18:02:06 +0800

    scala

---
+++ hello/src/main/scala/com/scala/ScalaApp.scala
@@ -0,0 +1,5 @@
+package com.scala
+
+object ScalaApp extends App {
+  println("ScalaApp")
+}
--- hello/src/main/scala/com/scala/string/Str.scala
+++ hello/src/main/scala/com/scala/string/Str.scala
@@ -3,16 +3,28 @@ package com.scala.string
 /**
  * {@code @description:} ${description}
  */
-class Str {
-
-}
-
 object Str {
   def main(args: Array[String]): Unit = {
     println(s"2 + 2 = ${2 + 2}") // 2 + 2 = 4
     
     val x = -1
     println(s"x.abs = ${x.abs}") // x.abs = 1
+    
+    println(
+      """The essence of Scala:
+                   Fusion of functional and object-oriented
+                   programming in a typed setting.""")
+    // The essence of Scala:
+    //                    Fusion of functional and object-oriented
+    //                    programming in a typed setting.
+    
+    println(
+      """The essence of Scala:
+        |Fusion of functional and object-oriented
+        |programming in a typed setting.""".stripMargin)
+    // The essence of Scala:
+    // Fusion of functional and object-oriented
+    // programming in a typed setting.
   }
 }
 
