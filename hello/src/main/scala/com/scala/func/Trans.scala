502c15fdb4c9eb2a54e490899b88ac15da41c075
hcx <623363564@qq.com>
2024-12-03 00:26:24 +0800

    函数传递

--- hello/src/main/scala/com/scala/func/Trans.scala
+++ hello/src/main/scala/com/scala/func/Trans.scala
@@ -6,36 +6,63 @@ package com.scala.func
 object Trans {
   def main(args: Array[String]): Unit = {
     /* 作为值传递 */
+    // def func(): String = {
+    //   println("func")
+    //   String.valueOf(2)
+    // }
+    //
+    // // 两种方式
+    // val result1: () => String = func
+    // val result2 = func _
+    //
+    // println(result1())
+    // println(result2())
+    //
+    // // -----------
     // def func(n: Int): String = {
     //   println("func")
     //   String.valueOf(n * 2)
     // }
     //
     // // 两种方式
-    // val result1 = func _
-    // val result2: Int => String = func
+    // val result1: Int => String = func
+    // val result2 = func _
     //
     // println(result1(2))
     // println(result2(2))
-
-
+    //
+    //
+    // // -----------
+    // def func(n: Int, m: Int): String = {
+    //   println("func")
+    //   String.valueOf(n * m * 2)
+    // }
+    //
+    // // 两种方式
+    // val result1: (Int, Int) => String = func
+    // val result2 = func _
+    //
+    // println(result1(2, 2))
+    // println(result2(2, 2))
+    
+    
     /* 作为参数传递 */
     def func(f: (Int, Int) => Int, a: Int, b: Int): Int = {
       f(a, b)
     }
-
+    
     // 以上函数可以接收以下函数作为参数
     def add(a: Int, b: Int): Int = a + b
-
+    
     def sub(a: Int, b: Int): Int = a - b
-
+    
     def mul(a: Int, b: Int): Int = a * b
-
+    
     println(func(add, 1, 2)) // 3
     println(func(_ + _, 1, 2)) // 3
-
-
-    /* 作为函数的返回值返回 */
+    
+    
+    /* 作为其他函数的返回值进行返回 */
     // def func(): Int => String = {
     //   def f(n: Int): String = {
     //     String.valueOf(n)
